e04f9107aaebb1cddf896af83520c5690019459a
mydamyda <mydamyda1032@gmail.com>
2015-11-16 15:23:12 +0900

    add upload controller

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1130,7 +1130,7 @@ function statistic_sdk_error_rating($scope, $http) {
 
         var porting = [];
 
-        for(var i = 0; i < data.length / 2; i++) {
+        for(var i = 0; i < parseInt(data.length / 2); i++) {
             porting.push({
                 label : data[i].osversion,
                 data : data[i].count
@@ -1138,7 +1138,7 @@ function statistic_sdk_error_rating($scope, $http) {
         }
 
         var others_count = 0;
-        for(var i = data.length / 2; i < data.length; i++) {
+        for(var i = parseInt(data.length / 2); i < data.length; i++) {
             others_count += data[i].count;
         }
         porting.push({
@@ -1303,6 +1303,7 @@ function statistic_country_error_rating($scope, $http) {
 angular
     .module('honeyqa')
     .controller('statistic_class_error_rating', statistic_class_error_rating)
+    .controller('statistic_class_error_list', statistic_class_error_list)
 
 function statistic_class_error_rating($scope, $http) {
 
@@ -1312,23 +1313,26 @@ function statistic_class_error_rating($scope, $http) {
     }).then(function successCallback(response) {
         var data = JSON.parse(JSON.stringify(response.data));
 
+
         var porting = [];
 
-        for(var i = 0; i < data.length / 2; i++) {
+        for(var i = 0; i < parseInt(data.length / 2); i++) {
             porting.push({
-                label : data[i].osversion,
+                label : data[i].lastactivity,
                 data : data[i].count
-            })
+            });
         }
 
+
         var others_count = 0;
-        for(var i = data.length / 2; i < data.length; i++) {
-            others_count += data[i].count;
+        for(var i = parseInt(data.length / 2); i < data.length; i++) {
+            others_count += parseInt(data[i].count);
         }
         porting.push({
             label : 'others',
             data : others_count
-        })
+        });
+
 
         if ($('#statistic_class_error_rate').length) {
 
@@ -1351,314 +1355,47 @@ function statistic_class_error_rating($scope, $http) {
             });
         }
 
+
+
     }, function errorCallback(response) {
         console.log('error : ' + response);
     });
 }
 
+function statistic_class_error_list($scope, $http) {
+    $http({
+        method: 'GET',
+        url: 'https://honeyqa.io:8080/statistics/720/lastactivity'
+    }).then(function successCallback(response) {
+        var data = JSON.parse(JSON.stringify(response.data));
 
-function dashboardFlotCtrl($scope) {
-    var data1 = [
-        [gd(2015, 1, 1), 838], [gd(2015, 1, 2), 749], [gd(2015, 1, 3), 634], [gd(2015, 1, 4), 1080], [gd(2015, 1, 5), 850], [gd(2015, 1, 6), 465], [gd(2015, 1, 7), 453], [gd(2015, 1, 8), 646], [gd(2015, 1, 9), 738], [gd(2015, 1, 10), 899], [gd(2015, 1, 11), 830], [gd(2015, 1, 12), 789]
-    ];
 
-    var data2 = [
-        [gd(2015, 1, 1), 342], [gd(2015, 1, 2), 721], [gd(2015, 1, 3), 493], [gd(2015, 1, 4), 403], [gd(2015, 1, 5), 657], [gd(2015, 1, 6), 782], [gd(2015, 1, 7), 609], [gd(2015, 1, 8), 543], [gd(2015, 1, 9), 599], [gd(2015, 1, 10), 359], [gd(2015, 1, 11), 783], [gd(2015, 1, 12), 680]
-    ];
+        $scope.ac = data;
 
-    var series = new Array();
 
-    series.push({
-        data: data1,
-        bars: {
-            show : true,
-            barWidth: 24 * 60 * 60 * 12000,
-            lineWidth: 1,
-            fill: 1,
-            align: 'center'
-        },
-        label: 'Revenues'
-    });
-    series.push({
-        data: data2,
-        color: '#e84e40',
-        lines: {
-            show : true,
-            lineWidth: 3,
-        },
-        points: {
-            fillColor: "#e84e40",
-            fillColor: '#ffffff',
-            pointWidth: 1,
-            show: true
-        },
-        label: 'Orders'
-    });
 
-    $.plot("#graph-bar", series, {
-        colors: ['#03a9f4', '#f1c40f', '#2ecc71', '#3498db', '#9b59b6', '#95a5a6'],
-        grid: {
-            tickColor: "#f2f2f2",
-            borderWidth: 0,
-            hoverable: true,
-            clickable: true
-        },
-        legend: {
-            noColumns: 1,
-            labelBoxBorderColor: "#000000",
-            position: "ne"
-        },
-        shadowSize: 0,
-        xaxis: {
-            mode: "time",
-            tickSize: [1, "month"],
-            tickLength: 0,
-            // axisLabel: "Date",
-            axisLabelUseCanvas: true,
-            axisLabelFontSizePixels: 12,
-            axisLabelFontFamily: 'Open Sans, sans-serif',
-            axisLabelPadding: 10
-        }
-    });
-
-    var previousPoint = null;
-    $("#graph-bar").bind("plothover", function (event, pos, item) {
-        if (item) {
-            if (previousPoint != item.dataIndex) {
-
-                previousPoint = item.dataIndex;
-
-                $("#flot-tooltip").remove();
-                var x = item.datapoint[0],
-                    y = item.datapoint[1];
-
-                showTooltip(item.pageX, item.pageY, item.series.label, y );
-            }
-        }
-        else {
-            $("#flot-tooltip").remove();
-            previousPoint = [0,0,0];
-        }
+    }, function errorCallback(response) {
+        console.log('error : ' + response);
     });
 }
 
-function device_range() {
-    //range slider
-    $('.slider-range').noUiSlider({
-        range: [1, 30],
-        start: [10, 15],
-        step: 1,
-        connect: true,
-        slide: function(){
-            var values = $(this).val();
+angular
+    .module('honeyqa')
+    .controller('proguard_list', proguard_list)
 
-            $(this).next('span').text(
-                'Term range: ' + values[0] + ' - ' + values[1]
-            );
-        },
-        set: function(){
-            var values = $(this).val();
+function proguard_list($scope, $http) {
+    $http({
+        method: 'GET',
+        url: 'https://honeyqa.io:8080/proguard/1288'
+    }).then(function successCallback(response) {
+        var data = JSON.parse(JSON.stringify(response.data));
 
-            $(this).next('span').text(
-                'Term range: ' + values[0] + ' - ' + values[1]
-            );
-        }
+
+        $scope.pl = data;
+
+
+
+    }, function errorCallback(response) {
+        console.log('error : ' + response);
     });
-    $('.slider-range').val([10, 15], true);
-}
-
-
-
-function device_morris_circle() {
-    $(".knob").knob({
-        change : function (value) {
-            //console.log("change : " + value);
-        },
-        release : function (value) {
-            //console.log(this.$.attr('value'));
-            console.log("release : " + value);
-        },
-        cancel : function () {
-            console.log("cancel : ", this);
-        },
-        draw : function () {
-
-            // "tron" case
-            if(this.$.data('skin') == 'tron') {
-
-                var a = this.angle(this.cv)  // Angle
-                    , sa = this.startAngle		  // Previous start angle
-                    , sat = this.startAngle		 // Start angle
-                    , ea							// Previous end angle
-                    , eat = sat + a				 // End angle
-                    , r = 1;
-
-                this.g.lineWidth = this.lineWidth;
-
-                this.o.cursor
-                && (sat = eat - 0.3)
-                && (eat = eat + 0.3);
-
-                if (this.o.displayPrevious) {
-                    ea = this.startAngle + this.angle(this.v);
-                    this.o.cursor
-                    && (sa = ea - 0.3)
-                    && (ea = ea + 0.3);
-                    this.g.beginPath();
-                    this.g.strokeStyle = this.pColor;
-                    this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sa, ea, false);
-                    this.g.stroke();
-                }
-
-                this.g.beginPath();
-                this.g.strokeStyle = r ? this.o.fgColor : this.fgColor ;
-                this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sat, eat, false);
-                this.g.stroke();
-
-                this.g.lineWidth = 2;
-                this.g.beginPath();
-                this.g.strokeStyle = this.o.fgColor;
-                this.g.arc( this.xy, this.xy, this.radius - this.lineWidth + 1 + this.lineWidth * 2 / 3, 0, 2 * Math.PI, false);
-                this.g.stroke();
-
-                return false;
-            }
-        }
-    });
-}
-
-function sdk_flot_donut() {
-    // donut chart
-    if ($('#graph-flot-donut').length) {
-        var dataDonut = [
-            { label: "3.0",  data: 10},
-            { label: "4.0.2",  data: 30},
-            { label: "4.1",  data: 90},
-            { label: "4.2",  data: 70},
-            { label: "5.0.1",  data: 80},
-            { label: "6.0",  data: 110}
-        ];
-
-        $.plot('#graph-flot-donut', dataDonut, {
-            series: {
-                pie: {
-                    show: true,
-                    innerRadius: 0.5,
-                    label: {
-                        show: true,
-                    }
-                }
-            },
-            colors: ['#e84e40', '#ffc107', '#8bc34a', '#03a9f4', '#9c27b0', '#90a4ae'],
-            legend: {
-                show: false,
-            }
-        });
-    }
-}
-
-function sdk_flot_bar() {
-    graphBar = Morris.Bar({
-        element: 'sdk-flot-bar',
-        data: [
-            {x: '3.0', y: 3, z: 2, a: 3},
-            {x: '4.0.2', y: 3, z: 2, a: 3},
-            {x: '4.1', y: 3, z: 2, a: 3},
-            {x: '4.2', y: 3, z: 2, a: 3},
-            {x: '5.0.1', y: 2, z: null, a: 1},
-            {x: '6.0', y: 0, z: 2, a: 4},
-        ],
-        barColors: ['#8bc34a', '#ffc107', '#e84e40', '#03a9f4', '#9c27b0', '#90a4ae'],
-        xkey: 'x',
-        ykeys: ['y', 'z', 'a'],
-        labels: ['Y', 'Z', 'A'],
-        resize: true
-    });
-}
-
-function country_map() {
-    //WORLD MAP
-    $('#world-map-example').vectorMap({
-        map: 'world_merc_en',
-        backgroundColor: '#ffffff',
-        zoomOnScroll: false,
-        regionStyle: {
-            initial: {
-                fill: '#e1e1e1',
-                stroke: 'none',
-                "stroke-width": 0,
-                "stroke-opacity": 1
-            },
-            hover: {
-                "fill-opacity": 0.8
-            },
-            selected: {
-                fill: '#8dc859'
-            },
-            selectedHover: {
-            }
-        },
-        markerStyle: {
-            initial: {
-                fill: '#e84e40',
-                stroke: '#e84e40'
-            }
-        },
-        //markers: [
-        //    {latLng: [38.35, -121.92], name: 'Los Angeles - 23'},
-        //    {latLng: [39.36, -73.12], name: 'New York - 84'},
-        //    {latLng: [50.49, -0.23], name: 'London - 43'},
-        //    {latLng: [36.29, 138.54], name: 'Tokyo - 33'},
-        //    {latLng: [37.02, 114.13], name: 'Beijing - 91'},
-        //    {latLng: [-32.59, 150.21], name: 'Sydney - 22'},
-        //],
-        series: {
-            regions: [{
-                values: gdpData,
-                scale: ['#6fc4fe', '#2980b9'],
-                normalizeFunction: 'polynomial'
-            }]
-        }
-        //,
-        //onRegionLabelShow: function(e, el, code){
-        //    el.html(el.html()+' ('+gdpData[code]+')');
-        //}
-    });
-}
-
-function class_flot_donut() {
-    // donut chart
-    if ($('#class_flot_donut').length) {
-        var dataDonut = [
-            { label: "3.0",  data: 10},
-            { label: "4.0.2",  data: 30},
-            { label: "4.1",  data: 90},
-            { label: "4.2",  data: 70},
-            { label: "5.0.1",  data: 80},
-            { label: "6.0",  data: 110}
-        ];
-
-        $.plot('#class_flot_donut', dataDonut, {
-            series: {
-                pie: {
-                    show: true,
-                    innerRadius: 0.5,
-                    label: {
-                        show: true,
-                    }
-                }
-            },
-            colors: ['#e84e40', '#ffc107', '#8bc34a', '#03a9f4', '#9c27b0', '#90a4ae'],
-            legend: {
-                show: false,
-            }
-        });
-    }
-}
-
-
-function proguardList($scope, $http) {
-}
-
-function dsymList($scope, $http) {
 }
